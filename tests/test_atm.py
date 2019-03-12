346f47e583cacb394b74524235bc0e03133b53b7
JeanCarloMachado <contato@jeancarlomachado.com.br>
2019-03-12 06:54:53 +0100

    MILESTONE first functional test

--- tests/test_atm.py
+++ tests/test_atm.py
@@ -1,4 +1,6 @@
+import os
 import unittest
+import json
 from typing import Dict, List, Generic, NamedTuple
 
 Note = int
@@ -8,21 +10,38 @@ RequestedAmount = int
 NoteAvailability = NamedTuple('NoteAvailability', [('note', Note), ('quantity', Quantity)])
 
 
+def dict_to_note_availability(data: Dict) -> NoteAvailability:
+    return NoteAvailability(note=data['note'], quantity=data['quantity'])
+
+
 class Gateway:
-    def load_available_notes() -> List[NoteAvailability]:
+    def load_available_notes(self) -> List[NoteAvailability]:
         raise NotImplementedError("Should have implemented this")
 
 
+class FileGateway(Gateway):
+    def load_available_notes(self) -> List[NoteAvailability]:
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        handle = open(f'{current_path}/../data/atm.json')
+        result = json.loads(handle.read())
+        handle.close()
+        return list(map(dict_to_note_availability, result['available_notes']))
+
+
 class ATM:
+    def factory(di_component=None):
+        return ATM(FileGateway())
+
     def __init__(self, gateway):
         self._gateway = gateway
 
     def withdraw(self, amount: RequestedAmount):
-        ATM._withdraw(self._gateway.load_available_notes(), amount)
+        return ATM._withdraw(self._gateway.load_available_notes(), amount)
 
     @staticmethod
     def _withdraw(available_notes: List[NoteAvailability], amount: RequestedAmount):
         """ available_notes must be sorted from the highest value to the lowest """
+        print(" gandalf", available_notes)
         if available_notes is None:
             return []
         rest_amount = amount
@@ -94,4 +113,12 @@ class ATMIntegrationTest(unittest.TestCase):
         gateway = Gateway()
         gateway.load_available_notes = lambda: [NoteAvailability(note=1, quantity=1)]
         atm = ATM(gateway)
-        atm.withdraw(1)
+        result = atm.withdraw(1)
+        self.assertEquals(result, [(1, 1)])
+
+
+class ATMFunctionalTest(unittest.TestCase):
+    def test_simple_case(self):
+        atm = ATM.factory()
+        result = atm.withdraw(1)
+        self.assertEqual(result, [(1, 1)])
