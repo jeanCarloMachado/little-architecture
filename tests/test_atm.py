abf92f5df8f5477912a22984a9a6b47b9aedc40a
JeanCarloMachado <contato@jeancarlomachado.com.br>
2019-03-12 16:28:58 +0100

    retrieve notes composing price with different ones

--- tests/test_atm.py
+++ tests/test_atm.py
@@ -2,12 +2,25 @@ import unittest
 
 
 def withdraw(availble_notes, amount: int):
-    """ available notes is a datastructture of List[(note, quantityAvailable)] """
-    for note, quantity in availble_notes:
-        if amount == note:
-            return [(note, 1)]
+    """ available notes is a datastructture of List[(note, quantityAvailable)] - sorted by bigger note to smaller """
 
-    return []
+    rest = amount
+    result = []
+    for note, quantity in availble_notes:
+        if rest >= note:
+            number_of_notes = _get_number_of_notes_minor_than_amount(amount, note)
+            result.append((note, number_of_notes))
+            rest -= note * number_of_notes
+
+    return result
+
+
+def _get_number_of_notes_minor_than_amount(amount, note):
+    number_of_notes = 1
+    while amount >= (note * (number_of_notes + 1)):
+        number_of_notes += 1
+
+    return number_of_notes
 
 
 class TestAtm(unittest.TestCase):
@@ -21,3 +34,11 @@ class TestAtm(unittest.TestCase):
     def test_user_asks_5_and_the_bank_has_none_shoud_return_none(self):
         result = withdraw(availble_notes=[], amount=5)
         self.assertEqual(result, [])
+
+    def test_use_more_than_one_of_the_same(self):
+        result = withdraw(availble_notes=[(10, 2)], amount=20)
+        self.assertEqual(result, [(10, 2)])
+
+    # def test_aggregate_price_with_different_notes(self):
+    #     result = withdraw(availble_notes=[(10, 1), (5, 1)], amount=15)
+    #     self.assertEqual(result, [(10, 1), (5, 1)])
