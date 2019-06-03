48d19f8a02ff77c151a80e1ca4a77a942b52bbe5
JeanCarloMachado <contato@jeancarlomachado.com.br>
2019-06-03 13:44:45 +0200

    add availability

--- test/AtmTest.php
+++ test/AtmTest.php
@@ -6,46 +6,63 @@
  */
 class AtmTest extends \PHPUnit\Framework\TestCase
 {
+    static $allAvailable = [
+        500 => 10,
+        100 => 10,
+        50 => 10,
+        10 => 10,
+        5 => 10,
+    ];
     public function testWithdrawNothingThrowsError()
     {
         $this->expectException(InvalidArgumentException::class);
-        withdraw(null);
+        withdraw(self::$allAvailable, null);
     }
 
     public function testAskForExactNoteValue()
     {
-        $result = withdraw(5);
+        $result = withdraw(self::$allAvailable, 5);
         $this->assertEquals([5], $result);
-        $result = withdraw(10);
+        $result = withdraw(self::$allAvailable, 10);
         $this->assertEquals([10], $result);
     }
 
     public function testComposedValues()
     {
-        $result = withdraw(1000);
+        $result = withdraw(self::$allAvailable, 1000);
         $this->assertEquals([500, 500], $result);
-        $result = withdraw(65);
+        $result = withdraw(self::$allAvailable, 65);
         $this->assertEquals([50, 10, 5], $result);
     }
+
+
+    public function testAvailability()
+    {
+        $result = withdraw([50=>1, 10=>0, 5=> 3], 65);
+        $this->assertEquals([50, 5, 5, 5], $result);
+    }
+
 }
 
 
-function withdraw($amount) {
+function withdraw($availability, $amount) {
 
     if (!$amount) {
         throw new InvalidArgumentException();
     }
 
     $result = [];
-    $notes = [500, 50, 10, 5];
 
     $remainingValue = $amount;
-    foreach ($notes as $note) {
+    foreach ($availability as $note => $noteAvailability) {
         if (($remainingValue / $note) < 1)  {
             continue;
         }
 
         $quantityOfNotes = intval($remainingValue / $note);
+        if ($quantityOfNotes > $noteAvailability) {
+            $quantityOfNotes = $noteAvailability;
+        }
 
         $result = addNNotesToResult($result, $note, $quantityOfNotes );
 
@@ -64,3 +81,4 @@ function addNNotesToResult($result, $note, $quantityOfNotes) {
 
         return $result;
 }
+
